1c9863de7895d5f29d14af92093f966422418327
Barbara <barbeaboo@gmail.com>
2024-10-12 11:09:04 -0700

    fixed indentation that caused error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,19 +3,21 @@
 """
 Pascal's triangle
 """
+
+
 def pascal_triangle(n):
     """  returns list of lists of integers representing 
-    the Pascal’s triangle of n """
+    the Pascal triangle of n """
 
     # the first row is always 1
     pyramid = [[1]] 
     if n <= 0:
         return []
-    # run through the remaing n rows
+    # run through the remaining n rows
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(pyramid[i-1][j-1]+pyramid[i-1][j])
         row.append(1)
-    pyramid.append(row)
+        pyramid.append(row)
     return pyramid
